90120c15572a9519a8fb35453911e143f7c8188d
Matt Johnson <matt.johnson.dev@gmail.com>
2024-03-04 17:47:40 +0000

    Command line param to set host/port bind

--- index.ts
+++ index.ts
@@ -3,9 +3,28 @@ import {SimplePeerServer} from "./lib/SimplePeerServer";
 let hostname = "0.0.0.0"
 let port = 8003;
 
-console.log(`Running on ${hostname}:${port}`);
+for(let arg of process.argv) {
+  if(!arg.startsWith("--"))
+    continue;
+
+  let params = arg.substring(2).split("=");
+
+  if(params.length != 2)
+    continue;
+
+  if(params[0] == 'bind') {
+    let p1 = params[1].split(":");
+    hostname = p1[0];
+
+    if(p1.length > 1) {
+      port = parseInt(p1[1]);
+    }
+  }
+}
+
+console.log(`--- simple-peer-server running on ${hostname}:${port} ---`);
 
 const http = require('http');
 const server = http.createServer();
 const spServer = new SimplePeerServer(server, true);
-server.listen(8003, hostname);
+server.listen(port, hostname);
